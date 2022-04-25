bbf073e5d3a06c30320cc2de07e3dccc91307a1b
Pegoral123 <jhonathansilva2123@gmail.com>
2022-04-25 16:56:22 -0300

    25/02/22

--- src/components/logonCliente.vue
+++ src/components/logonCliente.vue
@@ -7,14 +7,24 @@
         <form action="" class="col-12 col-md-6 m-auto bg-whithe p-5">
           <div class="form-group">
             <label class="float-start"> E-mail </label>
-            <input type="email" id="email" class="form-control" />
+            <input
+              type="email"
+              id="email"
+              v-model="dadosUsuario.usuario"
+              class="form-control"
+            />
           </div>
 
           <div class="form-group">
             <span class="d-flex justify-content-between"
               ><label>Senha</label> <i>Esqueceu a senha?</i></span
             >
-            <input type="password" id="senha" class="form-control" />
+            <input
+              type="password"
+              id="senha"
+              v-model="dadosUsuario.senha"
+              class="form-control"
+            />
           </div>
         </form>
       </div>
@@ -39,9 +49,49 @@
 
 <script>
 import NavBar from "@/components/NavBar.vue";
+import UsuarioService from "@/services/usuario";
 export default {
   components: {
     NavBar,
+  },
+  data() {
+    return {
+      dadosUsuario: {
+        usuario: "",
+        senha: "",
+      },
+    };
+  },
+  mounted() {
+    //this.loadDoc("http://localhost/api/usuario/list", myFunction);
+    this.listarUsuarios();
+  },
+  methods: {
+    // function loadDoc(url, xFunction) {
+    //   const xhttp = new XMLHttpRequest();
+    //   xhttp.onload = function () {
+    //     xFunction(this);
+    //   };
+    //   xhttp.open("GET", url);
+    //   xhttp.send();
+    // }
+    // function myFunction(xhttp) {
+    //   console.log(xhttp.responseText);
+    // }
+    listarUsuarios() {
+      UsuarioService.list().then((res) => {
+        console.log(res);
+      });
+    },
+    loganUsuario() {
+      UsuarioService.login(this.dadosUsuario)
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    },
   },
 };
 </script>
